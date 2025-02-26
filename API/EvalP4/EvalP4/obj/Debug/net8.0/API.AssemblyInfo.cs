a5898ece2dd0ae1464c4f059919146b35aa60c47
Marjolaine <Marjolaine.roger@diiage.org>
2025-02-26 16:20:25 +0100

    Initial push to new repository

--- API/EvalP4/EvalP4/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/EvalP4/EvalP4/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aefec8994182281b7794330ad23c8049525ef73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a3479833f8a72d6ab3744ac8c1d1e56829fb88")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
